b8ac5c7c04695e1072771b2503bbdbbac965e984
Fernando Alves <67766657+f7iger@users.noreply.github.com>
2022-06-28 10:07:06 -0300

    Add files via upload

---
+++ leitura.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import time
+import os
+
+rounds = 9
+intervalo = 60 * 2
+msg1, msg2 = "Intervalo", "Fim\ do\ intervalo"
+i = 0
+
+def alerta(aviso):
+    sintaxe = "zenity --warning --text={}".format(aviso)
+    os.system(sintaxe)
+    
+while i <= rounds:
+    os.system('sleep 10m')
+    alerta(msg1)
+    time.sleep(intervalo)
+    alerta(msg2)
+    i += 1
